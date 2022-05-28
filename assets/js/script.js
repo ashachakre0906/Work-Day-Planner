bf442a47a1d5414258fdde0b5419d8cc937e7fc8
ashachakre0906 <chourpagar.asha@gmail.com>
2022-05-28 02:54:57 -0700

    modified index file corrected the space and retrieving data from local storage through getitem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,53 @@
 var dateTime = moment().format('dddd, MMMM Do - h:mm:ss a');
 $('#currentDay').text(dateTime);
 console.log(dateTime);
-//added event listener to the save button
-$(".saveBtn").on ("click",function(){
-    // event.preventDefault();
-    console.log(this);
-    var userInput = parseInt($(this).siblings(".description").val());
-    console.log(userInput);
-    var time = $(this).parent().attr("id");//returning all the div's
-    console.log(time);
-    localStorage.setItem(userInput,time);//saving the data into local storage
-    localStorage.getItem(time);//getting the data from local storage
-    // console.log(userInput);
-    // console.log(time);
- });
-
 function colorBlock() {
     var hourNow = moment().hour(); 
     $(".time-block").each(function() {
-        var hourTime = ($(this).attr("id").split('-')[1]);
-        console.log(typeof hourTime);
-        if(hourTime < hourNow) {
-            $(this).addClass("past");
-            $(this).removeClass("present");
-            $(this).removeClass("future");
+        var Time = $(this).attr("id");
+        console.log(typeof Time);//returns string
+        if(Time < hourNow) {
+            $(this).addClass("past").css("background-color", "rgb(210, 87, 98)");
         }
-        else if(hourTime === hourNow){
-            $(this).addClass("present")
+        else if(Time === hourNow){
+            $(this).addClass("present").css("background-color", "rgb(42, 87, 0))");
+            $(this).removeClass("past");
         }
         else {
-            $(this).addClass("future")
+            $(this).addClass("future").css("background-color", "rgb(0, 191, 255)");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
         }   
     });}
     colorBlock();
+ ////Added event listener to the save button   
+    $(".saveBtn").on("click",function(event){
+        // event.PreventDefault();
+        // console.log(this);
+        var text = ($(this).siblings(".description").val());
+        console.log(text);
+        var value = $(this).parent().attr("id");//returning all the div's has unique ids
+        console.log(value);
+        localStorage.setItem(value,text);//saving the data into local storage
+        // localStorage.getItem(time);//getting the data from local storage
+        // console.log(userInput);
+        // console.log(time);
+     });
+    //retrieving the stored data from local storage for each timeblock 
+    //targetting the id , class description
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-1"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-2"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-3"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-4"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-5"));
+    $("#hour-18 .description").val(localStorage.getItem("hour-6"));
 
-    //fetching the data from localStorage
+
+
+    
 
 
