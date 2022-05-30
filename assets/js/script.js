dd561ef9ae9bc225c370e43bf69c7c5293bccf63
ashachakre0906 <chourpagar.asha@gmail.com>
2022-05-30 14:10:11 -0700

    Checking the console log for few lines

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,37 @@
 var dateTime = moment().format('dddd, MMMM Do - h:mm:ss a');
 $('#currentDay').text(dateTime);
 console.log(dateTime);
-function colorBlock() {
+//for each method to loop over each time block
+function colorCoded() {
     var hourNow = moment().hour(); 
     $(".time-block").each(function() {
-        var Time = $(this).attr("id");
+        var Time = $(this).attr("id").split('-')[1];
+        console.log ($(this));
         console.log(typeof Time);//returns string
         if(Time < hourNow) {
-            $(this).addClass("past").css("background-color", "rgb(210, 87, 98)");
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
         }
-        else if(Time === hourNow){
-            $(this).addClass("present").css("background-color", "rgb(42, 87, 0))");
+        else if(Time == hourNow){
             $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
         }
         else {
-            $(this).addClass("future").css("background-color", "rgb(0, 191, 255)");
             $(this).removeClass("past");
             $(this).removeClass("present");
+            $(this).addClass("future");
         }   
     });}
-    colorBlock();
- ////Added event listener to the save button   
-    $(".saveBtn").on("click",function(handler){
-        // $(document).ready(handler);
-        // event.PreventDefault();
-        // console.log(this);
+    colorCoded();
+    //Added event listener to the save button   
+    $(".saveBtn").on("click",function(){
         var text = $(this).siblings(".description").val().trim();
         console.log(text);
-        var value = $(this).parent().attr("id");//returning all the div's has unique ids
+        var value = $(this).parent().attr("id");
         console.log(value);
         localStorage.setItem(value,text);//saving the data into local storage
-        // localStorage.getItem(time);//getting the data from local storage
-        // console.log(userInput);
-        // console.log(time);
      });
 
     //retrieving the stored data from local storage for each timeblock 
@@ -42,16 +41,11 @@ function colorBlock() {
     $("#hour-10 .description").val(localStorage.getItem("hour-10"));
     $("#hour-11 .description").val(localStorage.getItem("hour-11"));
     $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-1"));
-    $("#hour-14 .description").val(localStorage.getItem("hour-2"));
-    $("#hour-15 .description").val(localStorage.getItem("hour-3"));
-    $("#hour-16 .description").val(localStorage.getItem("hour-4"));
-    $("#hour-17 .description").val(localStorage.getItem("hour-5"));
-    $("#hour-18 .description").val(localStorage.getItem("hour-6"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#hour-18 .description").val(localStorage.getItem("hour-18"));
     };
     displayData();
-
-
-    
-
-
